502eb6ade46192654a9acd4ccd0f271ac1a0a66a
Jayaraj Awasthi <awasthijayaraj11@gmail.com>
2026-08-15 08:26:54 +0545

    add shadcn,lucide-react and complete hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,23 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
+import {ArrowRight} from "lucide-react"
 
 export default function Home() {
   return (
     <div className="flex min-h-screen justify-center items-center flex-col bg-white">
-      <h1>hello world</h1>
+         <main className="flex-1 ">
+        {/*Hero section */}
+        <section className="container mx-auto px-4 py-32">
+          <div className="mx-auto max-w-4xl text-center">
+            <h1 className="text-black mb-6 text-6xl font-bold">A betteer way to track your job application</h1>
+            <p className="text-muted-foreground mb-10 text-xl">Capture, organize, and manage your job search in one place</p>
+            <div className="flex flex-col items-center gap-4 ">
+              <Button size="lg" className="h-12 px-8 text-lg font-medium">Start for free <ArrowRight className="ml-2" /> </Button>
+              <p className="text-sm text-muted-foreground">Free forever. No credit card required</p>
+            </div>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
